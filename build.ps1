74d90cd174b40915cf361f0982c4bf57417d6bc1
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-10-30 17:10:19 +0000

    Create build.ps1

---
+++ build.ps1
@@ -0,0 +1,22 @@
+﻿#
+# SystemReadinessCore builder script
+# Created by Ken Hoo (mrkenhoo)
+#
+
+If (Test-Path -Path "build\")
+{
+    Remove-Item -Path "build\" -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+    dotnet publish "SystemReadinessCore.csproj" --nologo --self-contained --runtime win-x64 --configuration Release --output "build\"
+    dotnet test SystemReadinessCore.csproj --no-build --verbosity normal
+}
+else
+{
+    dotnet publish "SystemReadinessCore.csproj" --nologo --self-contained --runtime win-x64 --configuration Release --output "build\"
+    dotnet test SystemReadinessCore.csproj --no-build --verbosity normal -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+}
+
+Remove-Item -Path "bin\" -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
+Remove-Item -Path "obj\" -Recurse -ErrorAction SilentlyContinue -WarningAction SilentlyContinue | Out-Null
+
+Write-Host -NoNewLine 'Press any key to continue...'
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown')
