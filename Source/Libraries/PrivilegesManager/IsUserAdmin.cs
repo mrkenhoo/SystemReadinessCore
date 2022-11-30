b8bcaaad56b9c25645b4d2550e40398e9204c8fb
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-11-30 13:05:01 +0000

    Move IsUserAdmin from Management to Libraries

---
+++ Source/Libraries/PrivilegesManager/IsUserAdmin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace SystemReadinessCore.Management.PrivilegesManager
+{
+    public static class GetPrivileges
+    {
+        public static bool IsUserAdmin()
+        {
+            try
+            {
+                WindowsIdentity identity = WindowsIdentity.GetCurrent();
+                WindowsPrincipal principal = new(identity);
+                return principal.IsInRole(WindowsBuiltInRole.Administrator);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
