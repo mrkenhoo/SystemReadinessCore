191b29e0e40bbb1985693c5592238c4cd916e218
Ken Hoo <60463234+mrkenhoo@users.noreply.github.com>
2022-12-24 17:45:38 +0000

    Create AreModulesInstalled.cs

---
+++ Source/Utilities/ModulesManager/AreModulesInstalled.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace SystemReadinessCore.Libraries.ModulesManager
+{
+    public partial class GetModules
+    {
+        private static readonly string[] SourcePathList = Directory.GetFiles(SourcePath);
+        private static readonly string[] DestinationPathList = Directory.GetDirectories(DestinationPath);
+        private static bool ModulesAreInstalled { get; set; }
+
+        public static bool AreModulesInstalled()
+        {
+            try
+            {
+                foreach (string folder in DestinationPathList)
+                {
+                    if (!Directory.Exists($"{DestinationPathList}\\{SourcePathList}"))
+                    {
+                          ModulesAreInstalled = true;
+                    }
+                    else
+                    {
+                        ModulesAreInstalled = false;
+                    }
+                }
+
+                return ModulesAreInstalled;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                throw;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                throw;
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
